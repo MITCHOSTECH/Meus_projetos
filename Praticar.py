38160c1450e085a9ad42b34d349935a0012b89fc
Ricardo Calipimbuth Mendes da Costa <ricascosta1@gmail.com>
2026-07-29 12:22:24 +0100

    Adicinar exercícios

--- Praticar.py
+++ Praticar.py
@@ -1,29 +1,7 @@
-from random import randint
-from time import sleep
-
-def sortear(lista):
-    for c in range(0,5):
-        lista.append(randint(1, 10))
-    print(f'Os números sorteados são: ', end=" ")
-    for num in lista:
-        print(f"{num}", end=" ")
-        sleep(1)
-    print()
+def ajuda(com):
+    help(com)
 
 
 
-def SomarPar(lista):
-    soma = 0
-    print(f'Números paraes sorteados são: ', end=" ")
-    for num in lista:
-        if num % 2 == 0:
-            print(f"{num}", end=" ")
-            sleep(1)
-            soma += num
-    print(f',Soma dos números pares: [{soma}]')
-
-
-
-numeros = list()
-sortear(numeros)
-SomarPar(numeros)
+m = str(input(f'Digite Comando help: '))
+ajuda(m)
